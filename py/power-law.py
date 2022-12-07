b7e744cf7d3a552a87d9d44cca6b60481897bc49
Daniel Vartanian <danvartan@gmail.com>
2022-12-07 04:33:46 -0300

    Update 'power-law.py'

--- py/power-law.py
+++ py/power-law.py
@@ -9,86 +9,123 @@
 # Contemporary Physics, 46(5), 323-351. 
 # https://doi.org/10.1080/00107510500052444
 
-import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 import random
-# from scipy.interpolate import make_interp_spline
 
 def f(x, r, alpha): return(x * ((1 - r) ** (- 1 / (alpha - 1))))
 
-def pl(n = 10 ** 6, alpha = 2.5, x_min = 1):
+def pl_1(n = 10 ** 6, alpha = 2.5, x_min = 1):
     y = [f(x_min, random.uniform(0, 1), alpha) for i in range(n)]
-    # y = [random.paretovariate(alpha) for i in range(n)]
-    
+
     y = np.array(y)
-    bin_width = 0.1 # 0.1
+    bin_width = 0.1
     bin_list = [1 + (i * bin_width) for i in range(round(y.max() / bin_width))]
     y_binned = np.histogram(y, bins = bin_list, density = True)[0]
-    x_bins = np.array([1 + (i * bin_width) for i in range(1, len(y_binned) + 1)])
-    
-    # x_y_spline = make_interp_spline(x_bins, y_binned)
-    # x_ = np.linspace(x_bins.min(), x_bins.max(), 500)
-    # y_ = x_y_spline(x_)
-    # ax.plot(x_, y_)
+    x_bins = np.array(bin_list[0:(len(bin_list) - 1)])
     
     return([x_bins, y_binned])
 
-def plot_pl(x, y, fmt = "r-", xlabel = "$x$", ylabel = "Probability density", 
-            xlim = None, ylim = None, log_log = False, text_x_fct = 0.3,
-            text_y_fct = 0.8, text_s = "(a)"):
-    x = np.array(x)
+def pl_2(n = 10 ** 6, alpha = 2.5, x_min = 1):
+    y = [f(x_min, random.uniform(0, 1), alpha) for i in range(n)]
     y = np.array(y)
     
-    plt.clf()
-    fig, ax = plt.subplots()
-    ax.plot(x, y, fmt)
-    ax.set_xlabel(xlabel, fontsize = 10)
-    ax.set_ylabel(ylabel, fontsize = 10)
+    bin_width = 0.1
+    bin_list = [1, 1.1]
+    multiplier = 2.5 # 2 (> 1)
     
-    if xlim != None:
-        if len(xlim) == 1:
-            ax.set_xlim(xlim[0])
-        else:
-            ax.set_xlim(xlim[0], xlim[1])
+    while bin_list[-1] <= y.max():
+        bin_width = multiplier * bin_width
+        bin_list.append(bin_list[-1] + bin_width)
     
-    if ylim != None:
-        if len(ylim) == 1:
-            ax.set_ylim(xlim[0])
-        else:
-            ax.set_ylim(ylim[0], xlim[1])
+    bin_intervals = []
     
-    if log_log == True:
-        ax.set_xscale("log")
-        ax.set_yscale("log")
-        ax.ticklabel_format(axis = "x", style = "plain")
-        # ax.set_xticks([1.0, 10.0, 100.0])
-        # ax.set_xticklabels(["1", "10", "100"])
+    for i in range(len(bin_list)):
+        if i == len(bin_list) - 1:
+            pass
+        else:
+            bin_intervals.append([bin_list[i], bin_list[i + 1]])
     
-    if text_s != None:
-        if xlim == None:
-            x_max = x.max()
-        else:
-            x_max = x.max() if len(xlim) == 1 else xlim[1]
-        
-        if ylim == None:
-            y_max = y.max()
-        else:
-            y_max = y.max() if len(ylim) == 1 else ylim[1]
+    weights = []
+    
+    for i in y:
+        for j in bin_intervals:
+            if (i >= j[0] and i < j[1]):
+                weights.append((j[0] + j[1]) / 2)
+    
+    weights = np.array(weights)
+    y_binned = np.histogram(y, bins = bin_list, density = True,
+               weights = weights)[0]
+    # y_binned = y_binned / np.diff(bin_list)
+    x_bins = np.array(bin_list[0:(len(bin_list) - 1)])
+    
+    return([x_bins, y_binned])
 
-        ax.text(x = text_x_fct * x_max, y = text_y_fct * y_max, s = text_s, 
-                horizontalalignment = "right", verticalalignment = "bottom", 
-                fontsize = 10)
-            
+def plot_pl(x_1, y_1, x_2, y_2):
+    fmt = "r-"
+    linewidth = 0.75
+    plt.rcParams.update({'font.size': 8})
+    xlabel = "$x$"
+    ylabel = "Sample p. density"
+    
+    plt.clf()
+    fig, [[ax1, ax2], [ax3, ax4]] = plt.subplots(2, 2)
+    plt.subplots_adjust(left = 0.15, bottom = 0.15, right = 0.925, 
+                        top = 0.925, wspace = 0.6, hspace = 0.4)
+    
+    ax1.plot(x_1, y_1, fmt, linewidth = linewidth)
+    ax1.set_xlim(0, 8)
+    ax1.set_ylim(0)
+    ax1
+    ax1.set_xlabel(xlabel)
+    ax1.set_ylabel(ylabel)
+    ax1.text(x = 3, y = 1.06, s = "(a)", horizontalalignment = "left", 
+             verticalalignment = "bottom")
+    
+    ax2.plot(x_1, y_1, fmt, linewidth = linewidth)
+    ax2.set_xlim(1, 10 ** 2 + (0.1 * 10 ** 3))
+    ax2.set_xscale("log")
+    ax2.set_yscale("log")
+    # ax2.ticklabel_format(axis = "x", style = "plain")
+    # ax2.set_xticks([1.0, 10.0, 100.0])
+    # ax2.set_xticklabels(["1", "10", "100"])
+    ax2.set_xlabel(xlabel)
+    ax2.set_ylabel(ylabel)
+    ax2.text(x = 7.5, y = 0.10, s = "(b)", horizontalalignment = "left", 
+             verticalalignment = "bottom")
+    
+    ax3.plot(x_2, y_2, fmt, linewidth = linewidth)
+    ax3.set_xlim(1)
+    ax3.set_ylim(10 ** - 9)
+    ax3.set_xscale("log")
+    ax3.set_yscale("log")
+    # ax3.ticklabel_format(axis = "x", style = "plain")
+    # ax3.set_xticks([1.0, 10.0, 100.0])
+    # ax3.set_xticklabels(["1", "10", "100"])
+    ax3.set_xlabel(xlabel)
+    ax3.set_ylabel(ylabel)
+    ax3.text(x = 25, y = 0.05, 
+             s = "(c)", horizontalalignment = "left", 
+             verticalalignment = "bottom")
+    
+    ax4.plot(x_1, y_1, fmt, linewidth = linewidth)
+    ax4.set_xlim(1, 199)
+    ax4.set_xscale("log")
+    ax4.set_yscale("log")
+    # ax4.ticklabel_format(axis = "x", style = "plain")
+    # ax4.set_xticks([1.0, 10.0, 100.0])
+    # ax4.set_xticklabels(["1", "10", "100"])
+    ax4.set_xlabel(xlabel)
+    ax4.set_ylabel(ylabel)
+    ax4.text(x = 7.5, y = 0.10, s = "(b)", horizontalalignment = "left", 
+             verticalalignment = "bottom")
+    
     plt.show()
 
-data = pl(n = 10 ** 6, alpha = 2.5, x_min = 1)
+data_1 = pl_1(n = 10 ** 6, alpha = 2.5, x_min = 1)
+data_2 = pl_2(n = 10 ** 6, alpha = 2.5, x_min = 1)
 
-# (a)
-plot_pl(x = data[0], y = data[1], xlim = [0, 8], ylim = [0], log_log = False,
-        text_x_fct = 0.3, text_y_fct = 0.8, text_s = "(a)")
+plot_pl(x_1 = data_1[0], y_1 = data_1[1], x_2 = data_2[0], y_2 = data_2[1])
 
-# (b)
-plot_pl(x = data[0], y = data[1], xlim = [1, 199], log_log = True,
-        text_x_fct = 0.0245, text_y_fct = 0.08, text_s = "(b)")
+
 
